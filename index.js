71557e9504b197bc0704f613e28b1c172aa4ec89
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-07 19:51:07 -0300

    adiconar if else and else if na function compareNumbers

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function init(){
 
 
 function compareNumbers(){
-    Number(document.getElementById('inputBox').value)
+   const userNumber = Number(document.getElementById('inputBox').value)
+   userNumbers.push('' + userNumber)
+   document.getElementById('guesses').innerHTML = userNumbers
+
+
+   if(userNumber > computerNumber){
+    document.getElementById('textOutput').innerHTML = 'Seu número está muito alto'
+
+   }
+   else if(userNumber < computerNumber){
+    document.getElementById('textOutput').innerHTML = 'Seu número está muito baixo'
+
+   }
+   else{
+    document.getElementById('textOutput').innerHTML = 'Parabéns!!!'
+
+   }
 
 }
